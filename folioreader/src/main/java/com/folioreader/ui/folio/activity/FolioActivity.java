3d4cabb724c57a2f898bccf84cc7e9335acbec36
garakuta <ametoajisai@gmail.com>
2018-07-02 21:21:41 +0900

    Merge my fix to master.

--- folioreader/src/main/java/com/folioreader/ui/folio/activity/FolioActivity.java
+++ folioreader/src/main/java/com/folioreader/ui/folio/activity/FolioActivity.java
@@ -60,6 +60,9 @@ import org.readium.r2_streamer.server.EpubServer;
 import org.readium.r2_streamer.server.EpubServerSingleton;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -138,7 +141,10 @@ public class FolioActivity
         if (ContextCompat.checkSelfPermission(FolioActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(FolioActivity.this, Constants.getWriteExternalStoragePerms(), Constants.WRITE_EXTERNAL_STORAGE_REQUEST);
         } else {
-            setupBook();
+            if(setupBook() == false){
+                Toast.makeText(this, R.string.cant_open_file, Toast.LENGTH_LONG);
+                finish();
+            }
         }
 
         initToolbar(savedInstanceState);
@@ -186,27 +192,41 @@ public class FolioActivity
         overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_up);
     }
 
-    private void initBook(String mEpubFileName, int mEpubRawId, String mEpubFilePath, EpubSourceType mEpubSourceType) {
+    private Boolean initBook(String mEpubFileName, int mEpubRawId, String mEpubFilePath, EpubSourceType mEpubSourceType) {
+        Boolean result = false;
         try {
             int portNumber = getIntent().getIntExtra(Config.INTENT_PORT, Constants.PORT_NUMBER);
             mEpubServer = EpubServerSingleton.getEpubServerInstance(portNumber);
             mEpubServer.start();
             String path = FileUtil.saveEpubFileAndLoadLazyBook(FolioActivity.this, mEpubSourceType, mEpubFilePath,
                     mEpubRawId, mEpubFileName);
-            addEpub(path);
 
-            String urlString = Constants.LOCALHOST + bookFileName + "/manifest";
-            new MainPresenter(this).parseManifest(urlString);
+            if(addEpub(path)){
+                String urlString = Constants.LOCALHOST + URLEncoder.encode(bookFileName, "UTF-8") + "/manifest";
+                new MainPresenter(this).parseManifest(urlString);
 
+                result = true;
+            }
         } catch (IOException e) {
-            Log.e(LOG_TAG, "initBook failed", e);
         }
+
+        return result;
     }
 
-    private void addEpub(String path) throws IOException {
+    private Boolean addEpub(String path) throws IOException {
+
+        Boolean result = true;
+
         Container epubContainer = new EpubContainer(path);
-        mEpubServer.addEpub(epubContainer, "/" + bookFileName);
+        try{
+            mEpubServer.addEpub(epubContainer, "/" + URLEncoder.encode(bookFileName));
+        }
+        catch(Exception ex){
+            result = false;
+        }
         getEpubResource();
+
+        return result;
     }
 
     private void getEpubResource() {
@@ -511,11 +531,30 @@ public class FolioActivity
     public void onError() {
     }
 
-    private void setupBook() {
-        bookFileName = FileUtil.getEpubFilename(this, mEpubSourceType, mEpubFilePath, mEpubRawId);
-        initBook(bookFileName, mEpubRawId, mEpubFilePath, mEpubSourceType);
+    public String md5(String s) {
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            for (int i=0; i<messageDigest.length; i++)
+                hexString.append(String.format("%02x", messageDigest[i] & 0xff));
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 
+    private Boolean setupBook() {
+        bookFileName = md5(mEpubFilePath);
+        return initBook(bookFileName, mEpubRawId, mEpubFilePath, mEpubSourceType);
+    }
+    
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
